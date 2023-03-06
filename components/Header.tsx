2c612186b103bcaca26ebac7a3005c29d4148956
levinhkygoat <leky.developer@gmail.com>
2023-03-06 16:27:47 +0700

    add tailwind

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <h1 className="text-3xl font-bold underline mt-2">
+    Hello world!
+  </h1>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Header } from '@/components/Header'
 
 export default function Home() {
   return (
@@ -18,8 +14,9 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
+      <Header />
       <main>
-      </main>
+      </main> 
     </>
   )
 }
